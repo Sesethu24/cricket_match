1fdb6238db58e79d2b7331a69eaa883a85a93ca5
Sesethu24 <seshmomoza20@gmail.com>
2019-09-20 10:56:06 +0200

    installed mocha

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cricket_match
+
---
+++ test/cricket_match.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const cricket_match = require('../cricket_match');
+
+describe('cricket_match', function(){
+
+    //const match = cricket_match();
+
+     it('should ', function(){
+    
+        assert.equal(2, );
+     });
+});
